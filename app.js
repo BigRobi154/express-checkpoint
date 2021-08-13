661aa424b2c081b86d7ec258a63150582320f842
Cherokee Walters <cherokeewalters162@gmail.com>
2021-08-13 12:55:15 -0400

    added post

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const movies = require('./movies.json');
+const knex = require('knex')(require('./knexfile.js')['development']);
 
 const app = express();
 const port = 3000;
+//const PORT = process.env.PORT || 3000;
+app.use(express.json()) // for parsing application/json
 
 app.get('/', function(req, res) {
     res.status(200).send("This is the movie API please use /movies for a list of movies")
@@ -10,34 +12,58 @@ app.get('/', function(req, res) {
 
 app.get('/movies', function(req, res) {
   let title = req.query.title
-    console.log(title);
-
-    if(title != undefined){
-      res.status(200).send(title)
-    }
-    else {
-      res.status(200).send(movies)
-    }
-  });
+  if (title === undefined){
+    knex
+      .select('*')
+      .from('movies')
+      .then(data => res.status(200).json(data))
+      .catch(err =>
+        res.status(404).json({
+         message:
+            'The data you are looking for could not be found. Please try again'
+        })
+      );
+  }
+  else {
+    knex
+     .select('*')
+     .from('movies')
+     .where('title', title)
+     .then(results => {
+      if(results.length === 0){
+        res.status(404).send(`Movie not found with title of ${title}.`)
+      }
+      else {res.status(200).send(results)}
+    })
+  }
+});
 
 app.get('/movies/:id', function(req, res) {
   let id = parseInt(req.params.id);
   //console.log("type of id: ", typeof id, "type of number: ", typeof 0)
 
-  const movieToReturn = movies.find(movie => movie.id === id);
-  console.log("Movie to return: ", movieToReturn);
-
   if (isNaN(id) === true) {
     res.status(400).send("Not a valid id. Please use an integer.")
   }
-  else if (movieToReturn === undefined) {
-    res.status(404).send(`Movie not found with id of ${id}.`)
-  }
   else {
-    res.status(200).send(movieToReturn)
+    knex('movies').where('id', id).then(results => {
+      if(results.length === 0){
+        res.status(404).send(`Movie not found with id of ${id}.`)
+      }
+      else {res.status(200).send(results)}
+    })
   }
+});
 
+app.post('/movies', function(req, res) {
+  console.log(req.body);
+  knex('movies').insert(req.body)
+  .then(() => res.status(200).send('movie was sent'))
+  .catch(err => {
+    return res.status(500).send(err);
   });
+
+});
 
 app.listen(3000, () => {
   console.log(`Yo dawg call me up on dat sweet port: ${port}`)
--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: 'postgresql',
     connection: {
       host: '127.0.0.1',
       port: '5432',
